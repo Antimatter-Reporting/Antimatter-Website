8a831ce1cea3895b1d62f8c30be48b425c417a39
Jamie Pegg <jamiepegg@zoho.com>
2024-02-15 23:14:38 +0000

    Fix github link

--- src/components/shared/navigation/social-icons.jsx
+++ src/components/shared/navigation/social-icons.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export function SocialIcons() {
     return (
         <div className="flex flex-row">
-            <Link href="https://github.com/Antimatter-Reporting/Antimatter" target="_blank"><Button variant="ghost" size="icon"><Github /></Button></Link>
+            <Link href="https://github.com/Antimatter-Reporting" target="_blank"><Button variant="ghost" size="icon"><Github /></Button></Link>
             <Link href="https://x.com/" target="_blank"><Button variant="ghost" size="icon"><Twitter /></Button></Link>
         </div>
     )
